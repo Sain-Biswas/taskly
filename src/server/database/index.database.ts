ae1ca4e8e63f05ec3c05134c3898ef22f9fb899a
Sain-Biswas <sain.biswas614@gmail.com>
2025-09-06 19:41:00 +0530

    feat: integrate drizzle orm for typesafe and easy database access.

--- src/server/database/index.database.ts
+++ src/server/database/index.database.ts
@@ -1,1 +1,34 @@
 import "server-only";
+
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+import { env } from "@/constant/environment-variables/server.env";
+import * as schema from "@/server/database/index.schema";
+
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+  conn: Pool | undefined;
+};
+
+/**
+ * Generating a node postgres driver.
+ */
+const conn =
+  globalForDb.conn ??
+  new Pool({
+    connectionString: env.DATABASE_URL,
+  });
+
+/**
+ * Cache the database connection if creating for the first time.
+ */
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+/**
+ * Drizzle database connection object for accessing postgres database from the whole application.
+ */
+export const database = drizzle({ client: conn, schema });
